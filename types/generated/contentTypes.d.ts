a52f168f4fc3fc562913d9597c38d7d1a79960d4
gulay53 <gulay_beyazal24@erdogan.edu.tr>
2026-05-16 11:22:01 +0300

    Güncellenmiş paket, TypeScript ve içerik türü şeması dosyaları

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -452,6 +452,12 @@ export interface ApiMekanlarMekanlar extends Struct.CollectionTypeSchema {
           localized: true;
         };
       }>;
+    aciklama_en: Schema.Attribute.Blocks &
+      Schema.Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
