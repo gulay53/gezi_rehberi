a342654008cede0af741127a20d1813878d9be19
gulay53 <gulay_beyazal24@erdogan.edu.tr>
2026-06-07 12:05:23 +0300

    "feat: sayfa tasarimi dinamik alani ve tuyo kutusu eklendi"

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,21 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface GeziTuyoKutusu extends Struct.ComponentSchema {
+  collectionName: 'components_gezi_tuyo_kutusus';
+  info: {
+    displayName: 'Tuyo Kutusu';
+    icon: 'pinMap';
+  };
+  attributes: {
+    mesaj: Schema.Attribute.Blocks;
+    tip: Schema.Attribute.Enumeration<['Uyar', 'Tavsiye', 'Bilgi']>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'gezi.tuyo-kutusu': GeziTuyoKutusu;
+    }
+  }
+}
